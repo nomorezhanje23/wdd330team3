9b6475b69a7b238fc7b78a87d79f84d450df8f56
nomorezhanje23 <nomorezhanje23@gmail.com>
2023-12-09 00:47:51 +0200

    week 9 team 8 done done

--- src/js/ExternalServices.js
+++ src/js/ExternalServices.js
@@ -9,17 +9,17 @@ async function convertToJson(res) {
   }
 }
 
-export default class ExternalServices {
+export default class ExternalServices  {
   constructor() {
     //Using the API means we don't need to tie the dataSource to a specific category anymore. So we can remove this in the constructor.
     // this.category = category;
     // this.path = `../json/${this.category}.json`;
+   
   }
   getData(category) {
     // instead we will pass the category we want in here when we need it.
     return fetch(baseURL + `products/search/${category}`)
-      .then(convertToJson)
-      .then((data) => data.Result);
+      .then(convertToJson).then((data) => data.Result);
   }
   async findProductById(id) {
     return await fetch(baseURL + `product/${id}`).then(convertToJson)
@@ -29,10 +29,38 @@ export default class ExternalServices {
     const options = {
       method: 'POST',
       headers: {
-        'Content-Type': 'application/json',
+        'Content-Type': 'application/json'
       },
-      body: JSON.stringify(payload),
-    };
-    return await fetch(baseURL + 'checkout/', options).then(convertToJson);
+      body: JSON.stringify(payload)
+    }
+      return await fetch(baseURL + 'checkout/', options).then(convertToJson);
+  }
+  // make a request to the server for a login token.
+  // requires: { email: 'someemail', password: 'somepassword' }
+  // returns: a valid jwt token if the email and password are valid.
+  async loginRequest(user) {
+    const options = {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(user)
+    }
+    const response = await fetch(baseURL + 'login', options).then(convertToJson);
+    return response.accessToken;
+  }
+  // make a request to the server for the current orders
+  // requires: a valid token
+  // returns: a list of orders
+  async getOrders(token) {
+    const options = {
+      method: 'GET',
+      // the server will reject our request if we don't include the Authorization header with a valid token!
+      headers: {
+        'Authorization': `Bearer ${token}`
+      }
+    }
+    const response = await fetch(baseURL + 'orders', options).then(convertToJson);
+    return response;
   }
 }
